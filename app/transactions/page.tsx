226b778e255a510f5d8a5611021e7778c938b911
ezin1 <eziof.dev@gmail.com>
2024-11-06 16:40:57 -0300

    feat: add initial code for transactions page

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,9 +1,13 @@
-const TransactionsPage = () => {
-    return (
-        <div>
-            <h1>Transations</h1>
-        </div>
-    )
-}
+// import { db } from "../_lib/prisma";
+
+const TransactionsPage = async () => {
+  // const transactions = await db.transaction.findMany({});
+
+  return (
+    <div>
+      <div className="flex w-full justify-between"></div>
+    </div>
+  );
+};
 
 export default TransactionsPage;
