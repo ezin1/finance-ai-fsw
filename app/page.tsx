090cacc5b0a4f4065d9af102d333525c8524cbda
ezin1 <eziof.dev@gmail.com>
2024-11-06 09:38:14 -0300

    chore: add git hook setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,17 @@ import { UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-
 const Home = async () => {
-  const {userId} = await auth();
+  const { userId } = await auth();
 
   if (!userId) {
-    redirect("/login"); 
+    redirect("/login");
   }
   return (
-    <div className="h-full flex items-center justify-center">
-      <UserButton showName/>
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName />
     </div>
   );
-
 };
 
 export default Home;
