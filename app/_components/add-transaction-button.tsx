46ce0bee441b8f8780e9d5f890c61370b5633846
ezin1 <eziof.dev@gmail.com>
2024-11-07 16:09:41 -0300

    feat: create initial code for component add transaction button

---
+++ app/_components/add-transaction-button.tsx
@@ -0,0 +1,37 @@
+import { ArrowDownUpIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+
+const AddTransactionButton = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="rounded-full font-bold">
+          Adicionar Transação
+          <ArrowDownUpIcon />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Adicionar Transação</DialogTitle>
+          <DialogDescription>Insira as informações abaixo</DialogDescription>
+        </DialogHeader>
+
+        <DialogFooter>
+          <Button variant="outline">Cancelar</Button>
+          <Button>Adicionar</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AddTransactionButton;
