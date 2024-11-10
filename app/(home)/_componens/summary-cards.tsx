2a31f39aabbc8969ae196abff8f271b4cf3a9ecf
ezin1 <eziof.dev@gmail.com>
2024-11-10 16:38:58 -0300

    feat: add sumary cards

--- app/(home)/_componens/summary-cards.tsx
+++ app/(home)/_componens/summary-cards.tsx
@@ -1,4 +1,3 @@
-import { Card, CardContent, CardHeader } from "@/app/_components/ui/card";
 import {
   PiggyBankIcon,
   TrendingDownIcon,
@@ -6,35 +5,78 @@ import {
   WalletIcon,
 } from "lucide-react";
 import SummaryCard from "./summary-card";
+import { db } from "@/app/_lib/prisma";
 
-const SummaryCards = () => {
+interface SummaryCardsProps {
+  month: string;
+}
+
+const SummaryCards = async ({ month }: SummaryCardsProps) => {
+  const where = {
+    date: {
+      gte: new Date(`2024-${month}-01`),
+      lt: new Date(`2024-${month}-31`),
+    },
+  };
+
+  const depositsTotal = Number(
+    (
+      await db.transaction.aggregate({
+        where: { ...where, type: "DEPOSIT" },
+        _sum: {
+          amount: true,
+        },
+      })
+    )?._sum?.amount,
+  );
+
+  const investmentsTotal = Number(
+    (
+      await db.transaction.aggregate({
+        where: { ...where, type: "INVESTMENT" },
+        _sum: {
+          amount: true,
+        },
+      })
+    )?._sum?.amount,
+  );
+
+  const expensesTotal = Number(
+    (
+      await db.transaction.aggregate({
+        where: { ...where, type: "EXPENSE" },
+        _sum: {
+          amount: true,
+        },
+      })
+    )?._sum?.amount,
+  );
+
+  const balance = depositsTotal - (investmentsTotal + expensesTotal);
+
   return (
     <div className="space-y-6">
-      <Card>
-        <CardHeader className="flex-row items-center gap-2">
-          <WalletIcon size={16} />
-          <p className="text-white opacity-70">Saldo</p>
-        </CardHeader>
-        <CardContent>
-          <p className="text-4xl font-bold">R$2.700</p>
-        </CardContent>
-      </Card>
-
-      <div className="grid grid-cols-3">
+      <SummaryCard
+        icon={<WalletIcon size={16} />}
+        title="Saldo"
+        amount={balance}
+        size="large"
+      />
+      <div className="grid grid-cols-3 gap-6">
         <SummaryCard
-          icon={<PiggyBankIcon size={14} />}
-          title="Saldo"
-          amount={12500}
-        />
-        <SummaryCard
-          icon={<TrendingUpIcon size={14} />}
+          icon={<PiggyBankIcon size={16} />}
           title="Investido"
-          amount={2500}
+          amount={investmentsTotal}
         />
         <SummaryCard
-          icon={<TrendingDownIcon size={14} />}
-          title="Despesa"
-          amount={2500}
+          icon={<TrendingUpIcon size={16} className="text-primary" />}
+          title="Receita"
+          amount={depositsTotal}
+        />
+        <SummaryCard
+          icon={<TrendingDownIcon size={16} className="text-red-500" />}
+          title="Despesas"
+          amount={expensesTotal}
         />
       </div>
     </div>
