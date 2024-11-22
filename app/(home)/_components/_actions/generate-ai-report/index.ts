94c868d6626664e5c465bb0e109439c58e5647b8
ezin1 <eziof.dev@gmail.com>
2024-11-22 11:19:55 -0300

    fix: resolver order data

--- app/(home)/_components/_actions/generate-ai-report/index.ts
+++ app/(home)/_components/_actions/generate-ai-report/index.ts
@@ -67,7 +67,7 @@ export const generateAiReport = async ({ month }: GenerateAiReportSchema) => {
 
   const dashboardData = await getDashboard(month);
   // mandar as transações para o ChatGPT e pedir para ele gerar um relatório com insights
-  const content = `Gere um relatório com insights sobre as minhas finanças, com dicas e orientações de como melhorar minha vida financeira. As transações estão divididas por ponto e vírgula. A estrutura de cada uma é {DATA}-{TIPO}-{VALOR}-{CATEGORIA}. São elas:
+  const content = `Gere um relatório com insights sobre as minhas finanças, com dicas e orientações de como melhorar minha vida financeira. As transações estão divididas por ponto e vírgula. A estrutura de cada uma é {DATA}-{VALOR}-{TIPO}-{CATEGORIA}. São elas:
   ${transactions
     .map(
       (transaction) =>
